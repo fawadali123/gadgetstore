ce317e05336467a3e2869f0c349b2865d3a5e82a
Fawad Ali <fawad.thinkreliability@nxvt.com>
2015-11-18 21:34:12 +0500

    Completed part. i.e Enabling Session State in Web Api. Modified the WebApiConfig.cs to define some custom routes as the default routes were not working in some cases.

--- GadgetStore/Store/app/Controllers/gadgetStoreCtrl.js
+++ GadgetStore/Store/app/Controllers/gadgetStoreCtrl.js
@@ -1,9 +1,11 @@
 ﻿angular.module('gadgetsStore')
-    .constant('gadgetsUrl', 'http://localhost:your_port_here/api/gadgets')
-    .constant('ordersUrl', 'http://localhost:your_port_here/api/orders')
-    .constant('categoriesUrl', 'http://localhost:your_port_here/api/categories')
-    .controller('gadgetStoreCtrl', function ($scope, $http, $location, gadgetsUrl, categoriesUrl, ordersUrl, cart) {
-
+    .constant('gadgetsUrl', 'http://localhost:52674/api/gadgets')
+    .constant('ordersUrl', 'http://localhost:52674/api/orders')
+    .constant('categoriesUrl', 'http://localhost:52674/api/categories')
+    .constant('tempOrdersUrl',     'http://localhost:52674/api/sessions/temporders/gettemporders')
+    .constant('saveTempOrdersUrl', 'http://localhost:52674/api/sessions/temporders/savetemporder')                                    
+    .controller('gadgetStoreCtrl', function ($scope, $http, $location, gadgetsUrl, categoriesUrl, ordersUrl,tempOrdersUrl,saveTempOrdersUrl, cart) {
+        
         $scope.data = {};
 
         $http.get(gadgetsUrl)
@@ -25,11 +27,12 @@
         $scope.sendOrder = function (shippingDetails) {
             var order = angular.copy(shippingDetails);
             order.gadgets = cart.getProducts();
-            $http.post(ordersUrl, order)
+            $http.post(saveTempOrdersUrl, order)
             .success(function (data, status, headers, config) {
                 $scope.data.OrderLocation = headers('Location');
                 $scope.data.OrderID = data.OrderID;
                 cart.getProducts().length = 0;
+                $scope.saveOrder();
             })
             .error(function (error) {
                 $scope.data.orderError = error;
@@ -44,6 +47,31 @@
 
         $scope.checkoutComplete = function () {
             return $location.path() == '/complete';
+        }
+
+        $scope.saveOrder = function () {
+            var currentProducts = cart.getProducts();
+
+            $http.post(saveTempOrdersUrl, currentProducts)
+                .success(function (data, status, headers, config) {
+                }).error(function (error) {
+                }).finally(function () {
+                });
+        }
+
+        $scope.checkSessionGadgets = function () {
+            $http.get(tempOrdersUrl)
+            .success(function (data) {
+                if (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        var item = data[i];
+                        cart.pushItem(item);
+                    }
+                }
+            })
+            .error(function (error) {
+                console.log('error checking session: ' + error);
+            });
         }
 
     });
